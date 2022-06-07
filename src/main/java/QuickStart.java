739663cef57cd013fca47b162767233bda25d79e
Michał Kowalewski <m.kowalewski@outlook.com>
2022-06-07 19:54:23 +0200

    kek

--- src/main/java/QuickStart.java
+++ src/main/java/QuickStart.java
@@ -29,7 +29,7 @@ public class QuickStart {
         String predictionCol = "prediction";
 
         // Generate train data and predict data as DataStream.
-        List<String[]> records = new ArrayList<>();
+        List<Row> records = new ArrayList<>();
         boolean header = true;
         try(BufferedReader br = new BufferedReader(new FileReader("beers.csv"))){
             String line;
@@ -39,68 +39,39 @@ public class QuickStart {
                     continue;
                 }
                 String[] values = line.split(",");
-                records.add(values);
+                records.add(Row.of(values));
             }
         }
         catch (Exception ignored) {
 
         }
 
-        DataStream<String[]> immStream = env.fromCollection(records, new RowTypeInfo(
+        DataStream<Row> immStream = env.fromCollection(records, new RowTypeInfo(
                 new TypeInformation[]{
-                        Types.DOUBLE,
-                        Types.DOUBLE,
-                        Types.DOUBLE
+                        TypeInformation.of(DenseVector.class),
+                        Types.INT
                 },
                 new String[] {"Feat1", "Feat2", "Pred"}
         ));
-        DataStream<DenseVector> inputStream = immStream
-                .filter(new FilterFunction<String[]>() {
+
+        DataStream<Row> inputStream = immStream
+                .filter(new FilterFunction<Row>() {
             @Override
-            public boolean filter(String[] strings) throws Exception {
-                for (String s : strings)
-                    if (s.length() == 0)
+            public boolean filter(Row row) throws Exception {
+                for (int i = 0; i < row.getArity(); i++)
+                    if (row.getFieldAs(i).toString().length() == 0)
                         return false;
                 return true;
             }
         })
-                .map(new MapFunction<String[], DenseVector>() {
+                .map(new MapFunction<Row, Row>() {
                     @Override
-                    public DenseVector map(String[] strings) throws Exception {
-                        return Vectors.dense(Double.parseDouble(strings[1]), Double.parseDouble(strings[2]), Double.parseDouble(strings[7]));
+                    public Row map(Row row) throws Exception {
+                        return Row.of(Vectors.dense(Double.parseDouble(row.getFieldAs(1)), Double.parseDouble(row.getFieldAs(2))), Integer.parseInt(row.getFieldAs(7)));
                     }
                 });
 
-        inputStream.getType() =
-        Table input = tEnv.fromDataStream(inputStream).as(featuresCol)
+        Table input = tEnv.fromDataStream(inputStream);
 
-
-        // Convert data from DataStream to Table, as Flink ML uses Table API.
-        Table input = tEnv.fromDataStream(inputStream).as(featuresCol);
-
-        // Creates a K-means object and initialize its parameters.
-        KMeans kmeans = new KMeans()
-                .setK(2)
-                .setSeed(1L)
-                .setFeaturesCol(featuresCol)
-                .setPredictionCol(predictionCol);
-
-        // Trains the K-means Model.
-        KMeansModel model = kmeans.fit(input);
-
-        // Use the K-means Model for predictions.
-        Table output = model.transform(input)[0];
-
-        CloseableIterator<Row> it = output.execute().collect();
-        System.out.println(it.toString());
-
-
-        // Extracts and displays prediction result.
-        /*for (CloseableIterator<Row> it = output.execute().collect(); it.hasNext(); ) {
-            Row row = it.next();
-            DenseVector vector = (DenseVector) row.getField(featuresCol);
-            int clusterId = (Integer) row.getField(predictionCol);
-            System.out.println("Vector: " + vector + "\tCluster ID: " + clusterId);
-        }*/
     }
 }
